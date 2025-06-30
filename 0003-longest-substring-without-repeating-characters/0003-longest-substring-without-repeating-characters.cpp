0018879b7bcd8270d40c6c7281c5b7499f3e3288
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-06-30 15:18:10 +0800

    Time: 0 ms (100%), Space: 10.2 MB (99.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int max_len = 0, left = 0;
-    bitset<128> seen;
+        bitset<128> seen;
 
-    for (int right = 0; right < s.length(); ++right) {
-        while (seen[s[right]]) {
-            seen[s[left++]] = 0;
+        for (int right = 0; right < s.length(); ++right) {
+            while (seen[s[right]]) {
+                seen[s[left++]] = 0;
+            }
+            seen[s[right]] = 1;
+            max_len = max(max_len, right - left + 1);
         }
-        seen[s[right]] = 1;
-        max_len = max(max_len, right - left + 1);
-    }
-
-    return max_len;
+        return max_len;
     }
     
 };
