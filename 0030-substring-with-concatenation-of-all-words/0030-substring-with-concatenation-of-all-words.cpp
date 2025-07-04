fac0b3f65788acec9745e97c9ad849219456ba6c
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-04 11:32:24 +0800

    Time: 5 ms (99.25%), Space: 21.1 MB (97.97%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         const int n = s.size(), m = words.size(), w = words[0].size();
-        if (m>3000){
+        if (m>2000){
             // 将 words 中的元素放入 set，检查是否只包含一个单词
             unordered_set<string> wordss(words.begin(), words.end());
 
