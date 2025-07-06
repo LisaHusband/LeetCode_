28867669b7c786ab810d66358bfb10020e91271c
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-06 12:05:09 +0800

    Time: 7 ms (98.76%), Space: 23 MB (99.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    unsigned long long multiply(unsigned long long base, unsigned long long factor) {
+        return base * factor;  // 这里是快速乘法的一个简单实现
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // 每个字母对应一个质数，a对应2，b对应3，... 按顺序
-        vector<int> prime = {103, 3, 11, 5, 7, 2, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
-                             61, 67, 71, 73, 79, 83, 89, 97, 101};
+        // 每个字母对应一个质数，a对应107，b对应3，... 按顺序
+        vector<int> prime = {107, 3, 2, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59,
+                             61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
 
         unordered_map<unsigned long long, vector<string>> hash;
         
         for (auto& s : strs) {
             unsigned long long key = 1;
             for (char c : s) {
-                // 字符减去 'a'后面的字符，保证从1开始索引prime数组
-                int idx = c - 96;
-                key *= prime[idx];
+                int idx = c - 96;  // 字符减去 'a'，保证从0开始索引prime数组
+                key = multiply(key, prime[idx]);  // 快速乘法
             }
             hash[key].push_back(s);
         }
