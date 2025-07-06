33c5a8a7d4e7fe8af8fa090bba79d031dcf900b7
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-06 12:12:01 +0800

    Time: 11 ms (92.31%), Space: 23 MB (99.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-    unsigned long long multiply(unsigned long long base, unsigned long long factor) {
-        return base * factor;  // 这里是快速乘法的一个简单实现
+    // 快速乘法，通过指数来优化乘法运算
+    unsigned long long multiplyFast(unsigned long long base, int prime) {
+        unsigned long long result = base;
+        for (int i = 1; i < prime; i++) {
+            result *= prime;  // 通过指数优化乘法
+        }
+        return result;
     }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // 每个字母对应一个质数，a对应107，b对应3，... 按顺序
+        // 每个字母对应一个质数，a对应2，b对应3，... 按顺序
         vector<int> prime = {107, 3, 2, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59,
-                             61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
+                             61, 67, 71, 73, 79, 83, 89, 97, 101, 103 };
 
         unordered_map<unsigned long long, vector<string>> hash;
+        
+        // 快速乘法函数
+        auto mul = [](unsigned long long a, unsigned long long b) -> unsigned long long {
+            unsigned long long ans = 0;
+            while (b > 0) {
+                if (b & 1) {  // 如果 b 的最后一位是1，结果可以加上 a
+                    ans += a;
+                }
+                a += a;  // a 乘 2
+                b >>= 1; // b 右移一位
+            }
+            return ans;
+        };
         
         for (auto& s : strs) {
             unsigned long long key = 1;
             for (char c : s) {
-                int idx = c - 96;  // 字符减去 'a'，保证从0开始索引prime数组
-                key = multiply(key, prime[idx]);  // 快速乘法
+                int idx = c - 96; // 字符减去 'a'，保证从0开始索引 prime 数组
+                key = mul(key, prime[idx]); // 使用快速乘法
             }
             hash[key].push_back(s);
         }
