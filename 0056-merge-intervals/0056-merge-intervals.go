18da3e0ce52be70a066b34c577e2ca385646bdec
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-06 14:27:59 +0800

    Time: 2 ms (71.95%), Space: 8.3 MB (69.39%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.go
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -1,32 +1,19 @@
-func merge(intervals [][]int) [][]int {
-    if len(intervals) == 0 {
-        return [][]int{}
-    }
-
-    // 按起点排序
-    sort.Slice(intervals, func(i, j int) bool {
-        return intervals[i][0] < intervals[j][0]
-    })
-
-    merged := [][]int{}
-    currentStart, currentEnd := intervals[0][0], intervals[0][1]
-
-    for _, interval := range intervals[1:] {
-        start, end := interval[0], interval[1]
-        if start <= currentEnd {
-            // 有重叠，更新终点
-            if end > currentEnd {
-                currentEnd = end
-            }
-        } else {
-            // 无重叠，保存当前区间，更新起点终点
-            merged = append(merged, []int{currentStart, currentEnd})
-            currentStart, currentEnd = start, end
+/**
+作者：灵茶山艾府
+链接：https://leetcode.cn/problems/merge-intervals/solutions/2798138/jian-dan-zuo-fa-yi-ji-wei-shi-yao-yao-zh-f2b3/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+**/
+func merge(intervals [][]int) (ans [][]int) {
+    slices.SortFunc(intervals, func(p, q []int) int { return p[0] - q[0] }) // 按照左端点从小到大排序
+    for _, p := range intervals {
+        m := len(ans)
+        if m > 0 && p[0] <= ans[m-1][1] { // 可以合并
+            ans[m-1][1] = max(ans[m-1][1], p[1]) // 更新右端点最大值
+        } else { // 不相交，无法合并
+            ans = append(ans, p) // 新的合并区间
         }
     }
-
-    // 添加最后一个区间
-    merged = append(merged, []int{currentStart, currentEnd})
-
-    return merged
+    return
 }
+
