600c48e050d74bce384f8a4547b564f03999dd17
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-06 12:23:52 +0800

    Time: 12 ms (85.67%), Space: 23.1 MB (98.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,38 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
+    // 快速乘法：用位运算加速乘法过程
+    unsigned long long mul(unsigned long long a, unsigned long long b) {
+        unsigned long long ans = 0; // 定义一个返回值
+        while (b > 0) {
+            if (b & 1) {  // 如果 b 的当前位为 1
+                ans += a; // 将 a 加到 ans
+            }
+            a += a;  // a 乘 2
+            b >>= 1; // b 右移一位
+        }
+        return ans;
+    }
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // 每个字母对应一个质数，a对应2，b对应3，... 按顺序
+        // 每个字母对应一个质数，a 对应 107，b 对应 3，... 按顺序
         vector<int> prime = {107, 3, 2, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59,
-                             61, 67, 71, 73, 79, 83, 89, 97, 101, 103 };
+                             61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
 
         unordered_map<unsigned long long, vector<string>> hash;
-        
-        // 快速乘法函数
-        auto mul = [](unsigned long long a, unsigned long long b) -> unsigned long long {
-            unsigned long long ans = 0;
-            while (b > 0) {
-                if (b & 1) {  // 如果 b 的最后一位是1，结果可以加上 a
-                    ans += a;
-                }
-                a += a;  // a 乘 2
-                b >>= 1; // b 右移一位
-            }
-            return ans;
-        };
-        
+
         for (auto& s : strs) {
             unsigned long long key = 1;
             for (char c : s) {
-                int idx = c - 96; // 字符减去 'a'，保证从0开始索引 prime 数组
+                int idx = c - 96;  // 字符减去 'a'，保证从 0 开始索引 prime 数组
                 key = mul(key, prime[idx]); // 使用快速乘法
             }
-            hash[key].push_back(s);
+            hash[key].push_back(s); // 将字符串按 key 存入哈希表
         }
-        
+
         vector<vector<string>> result;
         for (auto& p : hash) {
-            result.push_back(move(p.second));
+            result.push_back(move(p.second)); // 将结果按哈希表的值存入结果中
         }
-        return result;
+
+        return result; // 返回最终的结果
     }
 };
+
