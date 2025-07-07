61afb029169620d3acde4842ea76fc5f064bfb02
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-08 02:32:37 +0800

    Time: 22 ms (99.47%), Space: 29.5 MB (40.51%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,16 +6,21 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> result;
+        // 预分配内存，最坏情况下，每次外层循环都会找到一个三元组
+        result.reserve(nums.size() / 3);  // 根据输入数组的大小预分配内存
         
         sort(nums.begin(), nums.end());  // 排序
         
         int n = nums.size();
-        if (nums[n - 1] < 0) {
+        
+
+        int *start = &nums[0], *end = &nums[n - 1]; // 使用指针代替索引
+        
+        if (*end < 0) {
             return result;
         }
 
-        int *start = &nums[0], *end = &nums[n - 1]; // 使用指针代替索引
-
+        
         for (int *i = start; i <= end; ++i) {
             // 剪枝1: 如果当前nums[i]大于0，那么后面的数字必然大于0
             // 直接结束循环，因为没有可能形成和为0的三元组
