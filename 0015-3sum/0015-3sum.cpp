f312661241779de38d57d59be1b8fa4c3cc09b89
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-08 01:59:56 +0800

    Time: 26 ms (99.44%), Space: 28.9 MB (90.27%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -10,31 +10,34 @@ public:
         sort(nums.begin(), nums.end());  // 排序
         
         int n = nums.size();
-        if (nums[n-1] < 0) {
-            return result;  // 如果最大数小于0，三元组和无法为0
+        if (nums[n - 1] < 0) {
+            return result;
         }
 
-        for (int i = 0; i < n; ++i) {
-            // 剪枝1: 如果当前 nums[i] > 0，直接结束循环
-            if (nums[i] > 0) break;
+        int *start = &nums[0], *end = &nums[n - 1]; // 使用指针代替索引
+
+        for (int *i = start; i <= end; ++i) {
+            // 剪枝1: 如果当前nums[i]大于0，那么后面的数字必然大于0
+            // 直接结束循环，因为没有可能形成和为0的三元组
+            if (*i > 0) break;
             // 剪枝2: 跳过重复的元素
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            // 剪枝3: 如果 nums[i] + nums[n-2] + nums[n-1] < 0，不可能有三元组和为0
-            if (nums[i] + nums[n - 2] + nums[n - 1] < 0) continue;
-            
-            int target = -nums[i];  // 剩余两个数的和应该为 -nums[i]
-            int *left = &nums[i + 1], *right = &nums[n - 1];  // 指针操作
-            
+            if (i != start && *i == *(i - 1)) continue;
+
+            // 剪枝3: 如果 nums[first] + nums[n-2] + nums[n-1] < 0，不可能有三元组和为0
+            if (*i + *(end - 1) + *end < 0) continue;
+
+            int target = -(*i);  // 剩余的两个数的和应该为 -nums[i]
+            int *left = i + 1, *right = end;
+
             while (left < right) {
                 int sum = *left + *right;
-                
                 if (sum == target) {
-                    result.push_back({nums[i], *left, *right});
-                    
+                    result.push_back({*i, *left, *right});
+
                     // 跳过重复的元素
                     while (left < right && *left == *(left + 1)) left++;
                     while (left < right && *right == *(right - 1)) right--;
-                    
+
                     // 移动指针
                     left++, right--;
                 } 
