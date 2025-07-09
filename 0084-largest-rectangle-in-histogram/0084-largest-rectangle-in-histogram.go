ba444e625a60c60903b35ec61e4170292248c24a
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-09 13:21:58 +0800

    Time: 11 ms (70.87%), Space: 14.3 MB (18.37%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.go
@@ -0,0 +1,54 @@
+
+package main
+
+import (
+	"fmt"
+)
+
+func largestRectangleArea(heights []int) int {
+	n := len(heights)
+	if n == 0 {
+		return 0
+	}
+
+	shortFromLeft := make([]int, n)  // 左边第一个比当前小的柱子下标
+	shortFromRight := make([]int, n) // 右边第一个比当前小的柱子下标
+
+	shortFromLeft[0] = -1
+	shortFromRight[n-1] = n
+
+	// 计算每个柱子左边第一个比它矮的位置
+	for i := 1; i < n; i++ {
+		p := i - 1
+		for p >= 0 && heights[p] >= heights[i] {
+			p = shortFromLeft[p]
+		}
+		shortFromLeft[i] = p
+	}
+
+	// 计算每个柱子右边第一个比它矮的位置
+	for i := n - 2; i >= 0; i-- {
+		p := i + 1
+		for p < n && heights[p] >= heights[i] {
+			p = shortFromRight[p]
+		}
+		shortFromRight[i] = p
+	}
+
+	// 计算最大面积
+	maxArea := 0
+	for i := 0; i < n; i++ {
+		width := shortFromRight[i] - shortFromLeft[i] - 1
+		area := heights[i] * width
+		if area > maxArea {
+			maxArea = area
+		}
+	}
+
+	return maxArea
+}
+
+// func main() {
+// 	heights := []int{2, 1, 5, 6, 2, 3}
+// 	fmt.Println(largestRectangleArea(heights)) // 输出 10
+// }
