af96fa62c116642463a46a33a2adc1e70f524aa6
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-09 13:01:28 +0800

    Time: 3 ms (98.46%), Space: 16.4 MB (70.88%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -7,75 +7,77 @@ using namespace std;
 
 class Solution {
 public:
-        unordered_map<string, bool> sta; // 记忆化存储
+    unordered_map<string, bool> sta; // 记忆化存储
 
-bool check(string s1, string s2) {
-            if (s1.size() != s2.size()) {
-                return false;
-            }
-            if (s1.size() <= 1) {
-                return s1 == s2;
-            }
+    bool check(char* s1, char* s2, int len) {
+        if (len <= 1) {
+            return *s1 == *s2;
+        }
 
-            if (sta.find(s1 + s2) != sta.end()) {
-                return sta[s1 + s2];
-            }
+        string key(s1, len);
+        key += string(s2, len);
 
-            // from left to right
-            unordered_map<char, int> mem;
-            for (int i = 0; i < s1.size(); ++i) {
-                mem[s1[i]]++;
-                mem[s2[i]]--;
+        if (sta.find(key) != sta.end()) {
+            return sta[key];
+        }
 
-                bool is_valid = true;
-                for (auto &entry : mem) {
-                    if (entry.second != 0) {
-                        is_valid = false;
-                        break;
-                    }
-                }
+        // from left to right
+        vector<int> mem(256, 0); // 使用一个数组代替unordered_map
+        for (int i = 0; i < len; ++i) {
+            mem[s1[i]]++;
+            mem[s2[i]]--;
 
-                if (is_valid) {
-                    if (i != s1.size() - 1) {
-                        if (check(s1.substr(0, i + 1), s2.substr(0, i + 1)) && check(s1.substr(i + 1), s2.substr(i + 1))) {
-                            sta[s1 + s2] = true;
-                            return true;
-                        }
-                    }
+            bool is_valid = true;
+            for (int j = 0; j < 256; ++j) {
+                if (mem[j] != 0) {
+                    is_valid = false;
+                    break;
                 }
             }
 
-            // from right to left
-            mem.clear();
-            for (int i = 0; i < s1.size(); ++i) {
-                mem[s1[i]]++;
-                mem[s2[s2.size() - i - 1]]--;
-
-                bool is_valid = true;
-                for (auto &entry : mem) {
-                    if (entry.second != 0) {
-                        is_valid = false;
-                        break;
-                    }
-                }
-
-                if (is_valid) {
-                    if (i != s1.size() - 1) {
-                        if (check(s1.substr(0, i + 1), s2.substr(s2.size() - i - 1)) && check(s1.substr(i + 1), s2.substr(0, s2.size() - i - 1))) {
-                            sta[s1 + s2] = true;
-                            return true;
-                        }
+            if (is_valid) {
+                if (i != len - 1) {
+                    if (check(s1, s2, i + 1) && check(s1 + i + 1, s2 + i + 1, len - i - 1)) {
+                        sta[key] = true;
+                        return true;
                     }
                 }
             }
+        }
 
-            sta[s1 + s2] = false;
+        // from right to left
+        fill(mem.begin(), mem.end(), 0); // 清空计数器
+        for (int i = 0; i < len; ++i) {
+            mem[s1[i]]++;
+            mem[s2[len - i - 1]]--;
+
+            bool is_valid = true;
+            for (int j = 0; j < 256; ++j) {
+                if (mem[j] != 0) {
+                    is_valid = false;
+                    break;
+                }
+            }
+
+            if (is_valid) {
+                if (i != len - 1) {
+                    if (check(s1, s2 + len - i - 1, i + 1) && check(s1 + i + 1, s2, len - i - 1)) {
+                        sta[key] = true;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        sta[key] = false;
+        return false;
+    }
+
+    bool isScramble(string ss1, string ss2) {
+        if (ss1.size() != ss2.size()) {
             return false;
         }
-    bool isScramble(string ss1, string ss2) {
-
         
-
-        return check(ss1, ss2);
+        return check(&ss1[0], &ss2[0], ss1.size());
     }
 };
