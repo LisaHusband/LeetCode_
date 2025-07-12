92a937062cc7b4a2815e8794ae265643a536ea5a
LisaHusband <100658244+LisaHusband@users.noreply.github.com>
2025-07-12 16:58:27 +0800

    Time: 7 ms (96.73%), Space: 18.6 MB (20.66%) - LeetHub

---
+++ 0126-word-ladder-ii/0126-word-ladder-ii.py
@@ -0,0 +1,88 @@
+from collections import defaultdict
+from collections.abc import Iterable
+from itertools import chain
+from typing import List
+
+
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        # 辅助函数：BFS实现
+        class BFS:
+            __slots__ = ('searched', 'searching', 'searching_next')
+
+            def __init__(self, length: int, searching: Iterable[int], is_searched: bool) -> None:
+                self.searched: list[list[int]] = [[] for _ in range(length)]
+                self.searching: set[int] = set(searching)
+                self.searching_next: set[int] = set()
+                if is_searched:
+                    for i in self.searching:
+                        self.searched[i].append(i)
+
+            def search_one_index(self, index_prev: int, i: int) -> None:
+                if not self.searched[i]:
+                    self.searching_next.add(i)
+                    self.searched[i].append(index_prev)
+                elif i in self.searching_next:
+                    self.searched[i].append(index_prev)
+
+            def search_indexes(self, index_prev: int, indexes: Iterable[int]) -> None:
+                for i in indexes:
+                    self.search_one_index(index_prev, i)
+
+            def switch_next(self) -> None:
+                self.searching, self.searching_next = self.searching_next, self.searching
+
+        # 处理结束条件
+        try:
+            wordList.remove(endWord)
+        except ValueError:
+            return []
+
+        # 如果 beginWord 和 endWord 只差一个字母，直接返回
+        if sum(1 for chr1, chr2 in zip(beginWord, endWord) if chr1 != chr2) == 1:
+            return [[beginWord, endWord]]
+
+        try:
+            wordList.remove(beginWord)
+        except ValueError:
+            pass
+
+        len_word = len(beginWord)
+        dict_indexes = defaultdict(list)
+
+        # 创建字典：每个单词的变形组合
+        for i, word in enumerate(wordList):
+            for j in range(len_word):
+                dict_indexes[f'{word[:j]}_{word[j + 1 :]}'].append(i)
+
+        wordList.extend([beginWord, endWord])
+        len_wordlist = len(wordList)
+
+        # 初始化BFS的两个方向
+        begin = BFS(len_wordlist, [-2], False)
+        end = BFS(len_wordlist, [-1], False)
+
+        # 双向BFS
+        for depth in range(1, len_wordlist + 2):
+            for _ in range(len(begin.searching)):
+                word = wordList[i := begin.searching.pop()]
+                begin.search_indexes(i, chain(*[dict_indexes[f'{word[:j]}_{word[j + 1 :]}'] for j in range(len_word)]))
+            begin.switch_next()
+
+            # 如果找到了交集
+            if intersection := begin.searching & end.searching:
+                if depth % 2 == 0:
+                    begin, end = end, begin
+                
+                # 回溯构建路径
+                res = [[i] for i in intersection]
+                while res[0][0] != -2:
+                    res = [[i] + row for row in res for i in begin.searched[row[0]]]
+                while res[0][-1] != -1:
+                    res = [row + [i] for row in res for i in end.searched[row[-1]]]
+                
+                return [[wordList[i] for i in row] for row in res]
+
+            begin, end = end, begin
+
+        return []
